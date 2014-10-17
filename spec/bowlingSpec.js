0ff11aeda5a9e2ebbff72f231dd32b5597d10f0f
Bernard Mordan <bernardmordan@gmail.com>
2014-10-17 16:07:07 +0100

    Frame and Player objects started

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,11 +1,28 @@
 describe('10 Pin Bowling', function() {
 
   beforeEach(function(){
-    frame = new Frame;
-  })
+    frame = new Frame(1);
+    player = new Player;
+  });
 
   it('has frames', function() {
-    expect(frame.number).toEqual(1)
-  })
+    expect(frame.number).toEqual(1);
+  });
 
-})
+  it('in a frame you can throw twice', function() {
+    expect(frame.bowl(player)).toEqual(player);
+  });
+
+});
+
+describe('Your score works like this', function(){
+
+  beforeEach(function(){
+    player = new Player;
+  });
+
+  it('even if you are Jesus you start with 0', function() {
+    expect(player.getScore()).toEqual(0);
+  }); 
+
+});
--- src/bowling.js
+++ src/bowling.js
@@ -1,3 +1,11 @@
-function Frame() {
-  this.number = 1
-}
+function Frame(n) {
+  this.number = n
+};
+function Player() {};
+
+Player.prototype.getScore = function() {
+  return 0; 
+};
+Frame.prototype.bowl = function(player) {
+  return player;
+};
