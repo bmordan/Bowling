b5f962cb882389615862c12ed1a2588a54d0a997
Bernard Mordan <bernardmordan@gmail.com>
2014-10-17 17:25:00 +0100

    tracking throws in a frame

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -1,6 +1,6 @@
 describe('10 Pin Bowling', function() {
 
-  beforeEach(function(){
+  beforeEach(function() {
     frame = new Frame(1);
     player = new Player;
   });
@@ -13,9 +13,20 @@ describe('10 Pin Bowling', function() {
     expect(frame.bowl(player)).toEqual(player);
   });
 
+  it('tracks your thows', function() {
+    frame.bowl(player)
+    expect(player.ballNumber).toEqual(1)
+  });
+
+  it('knows your goes for that frame are up', function() {
+    frame.bowl(player)
+    frame.bowl(player)
+    expect(player.ballNumber).toEqual(2)
+  });
+
 });
 
-describe('Your score works like this', function(){
+describe('Your score works like this', function() {
 
   beforeEach(function(){
     player = new Player;
@@ -23,6 +34,7 @@ describe('Your score works like this', function(){
 
   it('even if you are Jesus you start with 0', function() {
     expect(player.getScore()).toEqual(0);
-  }); 
+  });
+
 
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,11 +1,21 @@
+// PLAYER //
+function Player() {
+  this.frame = 1
+  this.score = 0
+  this.ballNumber = 0
+};
+Player.prototype.getScore = function() {
+  return this.score; 
+};
+Player.prototype.addToScore = function(n) {
+  this.score = this.getScore() + n
+  return this.getScore
+};
+
 function Frame(n) {
   this.number = n
 };
-function Player() {};
-
-Player.prototype.getScore = function() {
-  return 0; 
-};
 Frame.prototype.bowl = function(player) {
+  player.ballNumber += 1
   return player;
 };
