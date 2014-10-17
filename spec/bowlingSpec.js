e99c58cce77915acdf2f06a17b1d306d9f1b975f
Bernard Mordan <bernardmordan@gmail.com>
2014-10-17 18:23:37 +0100

    flowing on the frame have a way to build it up

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -12,6 +12,29 @@ describe('10 Pin Bowling', function() {
   it('in a frame you have 2 bowls', function() {
     player.bowl(frame)
     expect(frame.ball_1).toEqual(false)
+  });
+
+  it('then the frame is over',function() {
+    player.bowl(frame)
+    player.bowl(frame)
+    expect(frame.ball_2).toEqual(false)
+  });
+
+  it('if you strike your frame is over', function() {
+    frame._strike()
+    expect(frame.strike).toBe(true)
+    expect(frame.ball_2).toBe(false)
+  });
+
+  it('the frame logs the pins you down on each go', function() {
+    frame.pinsDowned(8)
+    expect(frame.open_1).toEqual(8)
+  });
+
+  it('knows when you have a spare', function() {
+    frame.pinsDowned(3)
+    frame.pinsDowned(7)
+    expect(frame.spare).toEqual(true)
   })
 
 
--- src/bowling.js
+++ src/bowling.js
@@ -1,7 +1,6 @@
 function Player() {
   this.game       = []
   this.score      = 0
-  this.ballNumber = 0
 };
 Player.prototype.getScore = function() {
   return this.score; 
@@ -12,11 +11,21 @@ Player.prototype.addToScore = function(n) {
 };
 Player.prototype.bowl = function(frame) {
   frame.ball_1 = !frame.ball_1
+  if(!frame.ball_1) frame.ball_2 = !frame.ball_2
 };
-
 
 function Frame(n) {
   this.number = n;
   this.ball_1 = true;
   this.ball_2 = true;
+  this.open_1 = 0;
+  this.open_2 = 0;
+  this.strike = false;
 };
+Frame.prototype._strike = function() {
+  this.ball_2 = false;
+  this.strike = true;
+};
+Frame.prototype.pinsDowned = function(pinsDowned) {
+  this.open_1 = pinsDowned;
+}
