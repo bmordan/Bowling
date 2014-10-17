700f81a587641ee04e669d0e97c2afb6533ae098
Bernard Mordan <bernardmordan@gmail.com>
2014-10-17 15:43:24 +0100

    No one fucks with the Jesus (The Big Lebowski)

---
+++ spec/bowlingSpec.js
@@ -0,0 +1,11 @@
+describe('10 Pin Bowling', function() {
+
+  beforeEach(function(){
+    frame = new Frame;
+  })
+
+  it('has frames', function() {
+    expect(frame.number).toEqual(1)
+  })
+
+})
