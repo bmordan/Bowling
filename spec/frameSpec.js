27b9ee81eb49410d28a9512d4a0bf6d337031f55
Bernard Mordan <bernardmordan@gmail.com>
2014-10-22 12:24:41 +0100

    droped the idea of closing frames

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,5 +1,7 @@
 describe('When you play 10 pin bowling', function() {
 
+  var frame;
+
   beforeEach(function() {
     frame = new Frame;
   })
@@ -21,7 +23,7 @@ describe('When you play 10 pin bowling', function() {
   it('these 2 are added to give you your frame score', function() {
     frame.throw1 = 3
     frame.throw2 = 6
-    expect(frame.score).toEqual(9)
+    expect(frame.score()).toEqual(9)
   })
 
   it('now you might get all 10 pins down', function() {
@@ -33,23 +35,23 @@ describe('When you play 10 pin bowling', function() {
   it('that gives you a spare', function() {
     frame.throw1 = 5
     frame.throw2 = 5
-    expect(frame.score).toEqual(0)    
+    expect(frame.score()).toEqual(0)    
   })
 
   it('But hay, you might get all 10 down in one go.', function() {
-    frame.add(10)
+    frame.scoreStrike()
     expect(frame.strike).toEqual(true)
   })
 
-  it('Strike! that closes your frame', function() {
-    frame.add(10)
-    expect(frame.close).toEqual(true)
+  it('then your frame has bonus points added to it', function() {
+    expect(frame.bonus).toEqual(0)
   })
 
-  it('your two throws close a frame', function() {
-    frame.add(0)
-    frame.add(0)
-    expect(frame.close).toEqual(true)
+  it('those bouns points are added to your frame total', function() {
+    frame.throw1 = 10
+    frame.throw2 = 0
+    frame.bonus  = 20    
+    expect(frame.score()).toEqual(30)
   })
 
 })
--- src/frame.js
+++ src/frame.js
@@ -4,28 +4,18 @@ function Frame () {
   this.throw2 = 0;
   this.spare  = true;
   this.strike = false;
-  this.close  = false;
+  this.bonus  = 0
 }
-
-Frame.prototype = {
-  get score() { return this.total() }
-}
-Frame.prototype.total = function() {
-  var total = this.throw1 + this.throw2
+Frame.prototype.score = function() {
+  var total = this.throw1 + this.throw2 + this.bonus
   if (total === 10) return 0
   return total
 }
 Frame.prototype.scoreStrike = function() {
   this.strike = true;
-  this.close  = true;
-}
-Frame.prototype.closed = function() {
-  if (this.balls  === 2 ) this.close = true
-  if (this.throw1 === 10) this.scoreStrike()
 }
 Frame.prototype.add = function(pins) {
   var ball = this.balls += 1
   if (ball === 1) this.throw1 = pins
   if (ball === 2) this.throw2 = pins
-  this.closed()
 }
